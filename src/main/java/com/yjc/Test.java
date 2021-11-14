b281936d6175f5bcca13f72a874a82ccbd147d75
yjc <673105182@qq.com>
2021-11-14 20:06:07 +0800

    master修改

--- src/main/java/com/yjc/Test.java
+++ src/main/java/com/yjc/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hot-fix commit!");
+        System.out.println("master write");
     }
 }
